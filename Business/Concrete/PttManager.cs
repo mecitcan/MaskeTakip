52eaf72b66c9d6d9b05a657967ed2f4d8bb16bbd
mecitcan <130603660+mecitcan@users.noreply.github.com>
2024-02-18 14:38:22 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService)//Constructor new yapıldığında çalışır
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            PersonManager personManager = new PersonManager();
+            if (_applicantService.Checkperson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske verilemedi.");
+            }
+        }
+
+        public void Givemask(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaraound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Engin";
+            person.LastName = "Demiroğ";
+            person.DateOfBirthYear = 1985;
+            person.NationalIdentity = 12345678910;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person);
+
+        }
+
+    }
+}
